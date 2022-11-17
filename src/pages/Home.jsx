bcdbc2fa64cfe383303272b1213beb3099d99a2e
Ana Juárez <lisa13suarez@gmail.com>
2022-11-17 00:13:42 -0600

    editando home typewrite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import AOS from 'aos'
 import React from 'react'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import TypeWriterEffect from 'react-typewriter-effect'
 import 'aos/dist/aos.css'
 
 
@@ -16,9 +15,7 @@ const Home = () => {
             <div className='information'>
                 <div data-aos='fade-up' data-aos-easing='ease-out-cubic' data-aos-duration='1000' className='informationPrincipal'>
                     <p> {t('principal.I-am')} </p>
-                    <TypeWriterEffect startDelay={1000} cursorColor='#3F3D56'
-                    multiText={[ 'Ana Juárez!', 'Developer!', 'Front End!']}
-                    multiTextDelay={2000} typeSpeed={40}/>
+                   
                 </div>
             </div>
         </div>
