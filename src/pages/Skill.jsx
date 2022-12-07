2d0a1bcb8bceb9936498e2419a518374eca4921f
Ana Juárez <lisa13suarez@gmail.com>
2022-12-07 17:59:37 -0600

    añadiendo skilss/estilos

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import '../styles/Skill.css'
 
 const Skill = () => {
     return (
-        <div>
-            <h1>Hi Ana</h1>
+        <div className='containerSkill'>
+            <div class='container_general'>
+                <div class='box'>
+                    <i className='fa-brands fa-html5'></i>
+                    <h5>HTML</h5>
+                    <h4>HTML</h4>
+                </div>
+                <div class='box'>
+                    <i className='fa-brands fa-css3-alt'></i>
+                    <h5>CSS</h5>
+                    <h4>CSS</h4>
+                </div>
+                <div class='box'>
+                    <i className='fa-brands fa-js'></i>
+                    <h5>Javascript</h5>
+                    <h4>JS</h4>
+                </div>
+                <div class='box'>
+                    <i className='fa-brands fa-bootstrap'></i>
+                    <h5>Bootstrap</h5>
+                    <h4>Bootstrap</h4>
+                </div>
+                <div class='box'>
+                    <i className='fa-brands fa-react'></i>
+                    <h5>React JS</h5>
+                    <h4>React </h4>
+                </div>
+                <div class='box'>
+                    <i className='fa-brands fa-github'></i>
+                    <h5>GIT</h5>
+                    <h4>GIT</h4>
+                </div>
+            </div>
         </div>
     )
 }
