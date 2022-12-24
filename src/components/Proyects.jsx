19f7c8cfae290cad458a890dcc7214566412c5b9
Ana Juárez <lisa13suarez@gmail.com>
2022-12-23 20:39:03 -0600

    añadiendo proyectos extras

---
+++ src/components/Proyects.jsx
@@ -0,0 +1,139 @@
+import React from 'react'
+
+const Proyect = () => {
+    return (
+        <>
+            <div className="blog-card margin">
+                <div className="meta">
+                    <div className="photo" style={{backgroundImage: "url(https://i.postimg.cc/136bTWk1/clima.jpg)"}}></div>
+                </div>
+                <div className="description">
+                    <h1>Aplicación del Clima</h1>
+                    <h2>Muestra el clima según GPS, en grados Celcius y Farenheit</h2>
+                    <p></p>
+                    <div className='imagenesLogos'>
+                        <img src="https://i.postimg.cc/bN73hhnV/css3.png" alt="" className='css'/>
+                        <img src="https://i.postimg.cc/fLPQQfdh/logo-react-js.png" alt="" className='reactjs' />
+                        <img src="https://i.postimg.cc/15mR0JKv/Axios-logo.png" alt="" />
+                    </div>
+                    <div className='butonsLinks'>
+                        <button><a href="https://github.com/Layeska/Weather-Api.git" target={"_blank"}>Ver Código</a></button>
+                        <button><a href="https://app-ana.netlify.app/" target={"_blank"}>Ver Página</a></button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="blog-card ">
+                <div className="meta">
+                    <div className="photo" style={{backgroundImage: "url(https://i.postimg.cc/Jnbbb52g/Screenshot-2022-10-19-232900.png)"}}></div>
+                </div>
+                <div className="description">
+                    <h1>PokeApi</h1>
+                    <h2>Muestra detalles de los pokémones</h2>
+                    <p></p>
+                    <div className='imagenesLogos'>
+                        <img src="https://i.postimg.cc/bN73hhnV/css3.png" alt="" className='css'/>
+                        <img src="https://i.postimg.cc/fLPQQfdh/logo-react-js.png" alt="" className='reactjs' />
+                        <img src="https://i.postimg.cc/15mR0JKv/Axios-logo.png" alt="" />
+                    </div>
+                    <div className='butonsLinks'>
+                        <button><a href="https://github.com/Layeska/PokeAppi.git" target={"_blank"}>Ver Código</a></button>
+                        <button><a href="https://poke-appi-ana.netlify.app/" target={"_blank"}>Ver Página</a></button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="blog-card ">
+                <div className="meta">
+                    <div className="photo" style={{backgroundImage: "url(https://i.postimg.cc/65Z67t03/users-React.jpg)"}}>
+                    </div>
+                </div>
+                <div className="description">
+                    <h1>Visor de Usuarios</h1>
+                    <h2>Muestro una lista de usuarios de API con un estilo de morfismo de vidrio</h2>
+                    <p></p>
+                    <div className='imagenesLogos'>
+                        <img src="https://i.postimg.cc/bN73hhnV/css3.png" alt="" className='css'/>
+                        <img src="https://i.postimg.cc/fLPQQfdh/logo-react-js.png" alt="" className='reactjs' />
+                        <img src="https://i.postimg.cc/15mR0JKv/Axios-logo.png" alt="" />
+                    </div>
+                    <div className='butonsLinks'>
+                        <button><a href="https://github.com/Layeska/Users-json-React.git" target={"_blank"}>Ver Código</a></button>
+                        <button><a href="https://users-ana.netlify.app/" target={"_blank"}>Ver Página</a></button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="blog-card ">
+                <div className="meta">
+                    <div className="photo" style={{backgroundImage: "url(https://i.postimg.cc/yY5HR84Z/shoopad.jpg)"}}>
+                    </div>
+                </div>
+                <div className="description">
+                    <h1>Ecommerce Shoppad</h1>
+                    <h2>Se muestra detalles del producto, carrito de compra y recibo de compra. Se filtra por categoria y se puede buscar por nombre de producto</h2>
+                    <p></p>
+                    <div className='imagenesLogos'>
+                        <img src="https://i.postimg.cc/BvWmD3Yr/Bootstrap.png" alt="" className='bootstrap'/>
+                        <img src="https://i.postimg.cc/fLPQQfdh/logo-react-js.png" alt="" className='reactjs' />
+                        <img src="https://i.postimg.cc/15mR0JKv/Axios-logo.png" alt="" />
+                        <img src="https://i.postimg.cc/nhc03zY7/Redux.png" alt="" className='bootstrap'/>
+                    </div>
+                    <div className='butonsLinks'>
+                        <button><a href="https://github.com/Layeska/ecommerce-React.git" target={"_blank"}>Ver Código</a></button>
+                        <button><a href="https://ecommerce-react-ana.netlify.app/" target={"_blank"}>Ver Página</a></button>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="blog-card ">
+                <div className="meta">
+                    <div className="photo" style={{backgroundImage: "url(https://i.postimg.cc/0Qd62tJ0/screen.jpg)"}}>
+                    </div>
+                </div>
+                <div className="description">
+                    <h1>APPI Flowers</h1>
+                    <h2>Se crea usuarios, productos, agregar a carrito. Manda un correo de bienvenida, se usa paginación y rutas protegidas</h2>
+                    <p></p>
+                    <div className='imagenesLogos flowers'>
+                        <img src="https://i.postimg.cc/C5v84CFZ/node.png" alt="" className='css'/>
+                        <img src="https://i.postimg.cc/qqZtKkLy/Express-JS.png" alt="" className='reactjs' />
+                        <img src="https://i.postimg.cc/sg3X2Gyq/sequelize.png" alt="" className='sequelize' />
+                        <img src="https://i.postimg.cc/bv2KcLWh/postgresql.png" alt="" className='sequelize' />
+                        <img src="https://i.postimg.cc/x8ssbbh5/swagger.png" alt="" className='sequelize'/>
+                        <img src="https://i.postimg.cc/gcBxcH27/jest.png" alt="" className='sequelize'/>
+                    </div>
+                    <div className='butonsLinks'>
+                        <button><a href="https://github.com/Layeska/Flowers-Ecommerce.git" target={"_blank"}>Ver Código</a></button>
+                        <button><a href="#" target={"_blank"}>Ver Demo</a></button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="blog-card ">
+                <div className="meta">
+                    <div className="photo" style={{backgroundImage: "url(https://i.postimg.cc/FsH5xcSK/saine.jpg)"}}>
+                    </div>
+                </div>
+                <div className="description">
+                    <h1>Ecommerce Saine</h1>
+                    <h2>Proyecto realizado con un grupo conformado por roles: UX/UI, Back-End, Front-End. Yo participé en la elaboración del Front-End</h2>
+                    <ps></ps >aine
+                    <div className='imagenesLogos saine'>
+                        <img src="https://i.postimg.cc/bN73hhnV/css3.png" alt="" className='css'/>
+                        <img src="https://i.postimg.cc/fLPQQfdh/logo-react-js.png" alt="" className='reactjs' />
+                        <img src="https://i.postimg.cc/BvWmD3Yr/Bootstrap.png" alt="" className='bootstrap'/>
+                        <img src="https://i.postimg.cc/15mR0JKv/Axios-logo.png" alt="" />
+                    </div>
+                    <div className='butonsLinks'>
+                        <button><a href="https://github.com/No-Country/c8-86-t-react-firebase.git" target={"_blank"}>Ver Código</a></button>
+                        <button><a href="https://saine-clone.netlify.app/" target={"_blank"}>Ver Página</a></button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Proyect
